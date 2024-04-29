1b38fcf69c6531a310f9a3d9bcc91a1c1ef89f52
83336 <halshamlan@yahoo.com>
2024-04-29 15:21:59 +0300

    Add project files.

---
+++ BlazorApp1/Model/Entity/ApplicationState.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp1.Model.Entity
+{
+    public class ApplicationState
+    {
+        public int CurrentCount { get; set; } = 1;
+        public void Increment()
+        {
+
+        }
+    }
+}
---
+++ BlazorApp1/Model/TodoContext.cs
@@ -0,0 +1,22 @@
+﻿using BlazorApp1.Model.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Model
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) 
+        { 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().HasData(new List<TodoItem>{
+             new TodoItem{ Description = "Complete all Coded Assignments", IsCompleted = false, Id = 1, DueDate = new DateTime(2024,04,30)},
+             new TodoItem{ Description = "Appartment Rent", IsCompleted = true, Id = 2},
+             new TodoItem{ Description = "Clean My Room", IsCompleted = false, Id = 3 }
+ });
+
+        }
+    }
+}
